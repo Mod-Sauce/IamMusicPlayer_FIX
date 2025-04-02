d18d39190ff0cdf7e08b9a54a536145c19e28a9f
Us3r0 <modsauce@disroot.org>
2025-04-02 19:24:10 +0200

    Update IMPBlocks.java

--- common/src/main/java/dev/felnull/imp/block/IMPBlocks.java
+++ common/src/main/java/dev/felnull/imp/block/IMPBlocks.java
@@ -18,8 +18,8 @@ import java.util.function.Supplier;
 public class IMPBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(IamMusicPlayer.MODID, Registries.BLOCK);
     private static final DeferredRegister<Item> BLOCK_ITEMS = DeferredRegister.create(IamMusicPlayer.MODID, Registries.ITEM);
-    public static final RegistrySupplier<Block> MUSIC_MANAGER = register("music_manager", () -> new MusicManagerBlock(BlockBehaviour.Properties.of(Material.METAL).sound(SoundType.METAL).strength(3.0F).requiresCorrectToolForDrops()));
-    public static final RegistrySupplier<Block> CASSETTE_DECK = register("cassette_deck", () -> new CassetteDeckBlock(BlockBehaviour.Properties.of().sound(SoundType.METAL).strength(3.0F).requiresCorrectToolForDrops()));
+    public static final RegistrySupplier<Block> MUSIC_MANAGER = register("music_manager", () -> new MusicManagerBlock(BlockBehaviour.Properties.of(Material.METAL).requiresCorrectToolForDrops().sound(SoundType.METAL).strength(3.0F).requiresCorrectToolForDrops()));
+    public static final RegistrySupplier<Block> CASSETTE_DECK = register("cassette_deck", () -> new CassetteDeckBlock(BlockBehaviour.Properties.of().requiresCorrectToolForDrops().sound(SoundType.METAL).strength(3.0F).requiresCorrectToolForDrops()));
     public static final RegistrySupplier<Block> BOOMBOX = register("boombox", () -> new BoomboxBlock(BlockBehaviour.Properties.of().sound(SoundType.LANTERN).strength(3.0F)), n -> new BoomboxItem(n, new Item.Properties().arch$tab(IMPCreativeModeTabs.MOD_TAB).stacksTo(1)));
 
     private static RegistrySupplier<Block> register(String name, Supplier<Block> block) {
