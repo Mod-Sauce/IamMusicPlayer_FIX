59189311839ca038f00cc5860269bb121bfc1453
Shadowbee <169397554+Shadowbee27@users.noreply.github.com>
2025-05-27 13:35:33 +0200

    Formatted LavaNativeManager.java and added checks for PRs

--- common/src/main/java/dev/felnull/imp/client/lava/LavaNativeManager.java
+++ common/src/main/java/dev/felnull/imp/client/lava/LavaNativeManager.java
@@ -17,7 +17,8 @@ import java.nio.file.Files;
 import java.util.Map;
 
 public class LavaNativeManager {
-    private static final Logger LOGGER = LogManager.getLogger(LavaNativeManager.class);
+    private static final Logger LOGGER =
+            LogManager.getLogger(LavaNativeManager.class);
     private static final Gson GSON = new Gson();
     private static final LavaNativeManager INSTANCE = new LavaNativeManager();
     private static final String nativesVersion = "2.2.3";
@@ -27,7 +28,8 @@ public class LavaNativeManager {
     }
 
     public boolean load(String osAndArch, String name) {
-        var npF = LavaPlayerLoader.getNaiveLibraryFolder().resolve(osAndArch).toFile();
+        var npF =
+                LavaPlayerLoader.getNaiveLibraryFolder().resolve(osAndArch).toFile();
         if (!checked(npF)) {
             try {
                 LOGGER.info("LavaPlayer natives download start");
@@ -44,17 +46,22 @@ public class LavaNativeManager {
     }
 
     private void downloadNatives(String osAndArch) throws Exception {
-        var npF = LavaPlayerLoader.getNaiveLibraryFolder().resolve(osAndArch).toFile();
+        var npF =
+                LavaPlayerLoader.getNaiveLibraryFolder().resolve(osAndArch).toFile();
         if (!npF.exists() && !npF.mkdirs())
-            throw new IllegalStateException("Failed to create the folder of the native library");
+            throw new IllegalStateException(
+                    "Failed to create the folder of the native library");
 
         JsonObject jo;
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(FNURLUtil.getStream(new URL(IamMusicPlayer.getConfig().lavaPlayerNativesURL))))) {
+        try (BufferedReader reader =
+                     new BufferedReader(new InputStreamReader(FNURLUtil.getStream(
+                             new URL(IamMusicPlayer.getConfig().lavaPlayerNativesURL))))) {
             jo = GSON.fromJson(reader, JsonObject.class);
         }
 
         if (!jo.has(nativesVersion) || !jo.get(nativesVersion).isJsonObject())
-            throw new IllegalStateException("Could not find version of native library to support");
+            throw new IllegalStateException(
+                    "Could not find version of native library to support");
 
         var joo = jo.getAsJsonObject(nativesVersion);
 
@@ -74,14 +81,17 @@ public class LavaNativeManager {
 
         Files.writeString(npF.toPath().resolve("hash.json"), GSON.toJson(ho));
 
-        FNDataUtil.readZipStreamed(new BufferedInputStream(FNURLUtil.getStream(new URL(jooo.get("url").getAsString()))), (zipEntry, inputStream) -> {
-            var fl = npF.toPath().resolve(zipEntry.getName()).toFile();
-            try (InputStream is = inputStream; OutputStream os = new FileOutputStream(fl)) {
-                FNDataUtil.bufInputToOutput(is, os);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+        FNDataUtil.readZipStreamed(new BufferedInputStream(FNURLUtil.getStream(
+                        new URL(jooo.get("url").getAsString()))),
+                (zipEntry, inputStream) -> {
+                    var fl = npF.toPath().resolve(zipEntry.getName()).toFile();
+                    try (InputStream is = inputStream;
+                         OutputStream os = new FileOutputStream(fl)) {
+                        FNDataUtil.bufInputToOutput(is, os);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
 
         if (!checked(npF))
             throw new IllegalStateException("Consistency check failed");
@@ -89,10 +99,12 @@ public class LavaNativeManager {
 
     private boolean checked(File file) {
         var fs = file.listFiles();
-        if (fs == null) return false;
+        if (fs == null)
+            return false;
         var fls = Lists.newArrayList(fs);
         var hf = fls.stream().filter(n -> n.getName().equals("hash.json")).findAny();
-        if (hf.isEmpty()) return false;
+        if (hf.isEmpty())
+            return false;
         JsonObject jo;
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(hf.get())))) {
             jo = GSON.fromJson(reader, JsonObject.class);
@@ -106,19 +118,24 @@ public class LavaNativeManager {
         if (fls.size() == 1 && !jo.get("hash").isJsonObject()) {
             try {
                 var ch = jo.get("hash").getAsString();
-                var th = new String(Hex.encodeHex(FNDataUtil.createMD5Hash(Files.readAllBytes(fls.get(0).toPath()))));
+                var th = new String(Hex.encodeHex(
+                        FNDataUtil.createMD5Hash(Files.readAllBytes(fls.get(0).toPath()))));
                 return th.equals(ch);
             } catch (Exception ignored) {
             }
-        } else if (jo.get("hash").isJsonObject() && jo.get("hash").getAsJsonObject().size() == fls.size()) {
+        } else if (jo.get("hash").isJsonObject()
+                && jo.get("hash").getAsJsonObject().size() == fls.size()) {
             var hjo = jo.get("hash").getAsJsonObject();
             for (Map.Entry<String, JsonElement> entry : hjo.entrySet()) {
                 var lf = fls.stream().filter(n -> n.getName().equals(entry.getKey())).findAny();
-                if (lf.isEmpty()) return false;
+                if (lf.isEmpty())
+                    return false;
                 try {
                     var ch = entry.getValue().getAsString();
-                    var th = new String(Hex.encodeHex(FNDataUtil.createMD5Hash(Files.readAllBytes(lf.get().toPath()))));
-                    if (!th.equals(ch)) return false;
+                    var th = new String(Hex.encodeHex(
+                            FNDataUtil.createMD5Hash(Files.readAllBytes(lf.get().toPath()))));
+                    if (!th.equals(ch))
+                        return false;
                 } catch (Exception ignored) {
                     return false;
                 }
