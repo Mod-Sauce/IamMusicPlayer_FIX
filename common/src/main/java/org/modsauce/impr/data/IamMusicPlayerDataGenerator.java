9081462596a53d921b0940d458267a40284448b1
Shadowbee27 <shadowbee.contact@proton.me>
2025-11-20 18:14:42 +0100

    Refactor Gradle, bump deps, remove generated data
    Update Architectury Loom to 1.13-SNAPSHOT and bump dependency versions
    (otyacraft engine, lavalink). Lock Minecraft to 1.21.1 and compact
    fabric entrypoints. Fix .gitignore merge markers. Remove generated data
    files under fabric/src/main/generated. Refactor neoforge/build.gradle
    (shadowJar, sourcesJar and configuration cleanup) and tidy
    IamMusicPlayerDataGenerator formatting and providers

--- common/src/main/java/org/modsauce/impr/data/IamMusicPlayerDataGenerator.java
+++ common/src/main/java/org/modsauce/impr/data/IamMusicPlayerDataGenerator.java
@@ -1,24 +1,36 @@
 package org.modsauce.impr.data;
 
+import java.nio.file.Paths;
+import net.minecraft.data.PackOutput;
 import org.modsauce.impr.data.advancements.IMPAdvancementProviderWrapper;
 import org.modsauce.otyacraftenginerenewed.data.CrossDataGeneratorAccess;
 import org.modsauce.otyacraftenginerenewed.data.provider.DataProviderWrapper;
 import org.modsauce.otyacraftenginerenewed.data.provider.DirectCopyProviderWrapper;
-import net.minecraft.data.PackOutput;
-
-import java.nio.file.Paths;
 
 public class IamMusicPlayerDataGenerator {
-    public static void init(CrossDataGeneratorAccess access) {
-        access.addResourceInputFolders(Paths.get("../../resources"));
 
-        access.addProviderWrapper(IMPRecipeProviderWrapper::new);
-        var btp = access.addProviderWrapper(IMPBlockTagProviderWrapper::new);
+  public static void init(CrossDataGeneratorAccess access) {
+    access.addResourceInputFolders(Paths.get("../../resources"));
 
-        access.addProviderWrapper((DataProviderWrapper.LookupGeneratorAccessedFactory<DataProviderWrapper<?>>) (packOutput, lookup, generatorAccess) -> new IMPItemTagProviderWrapper(packOutput, lookup, generatorAccess, btp));
-        access.addProviderWrapper(IMPPoiTypeTagProviderWrapper::new);
-        access.addProviderWrapper(packOutput -> new DirectCopyProviderWrapper(packOutput, PackOutput.Target.DATA_PACK, "patchouli_books", access));
-        access.addProviderWrapper(IMPBlockLootTableProviderWrapper::new);
-        access.addProviderWrapper(IMPAdvancementProviderWrapper::new);
-    }
+    //access.addProviderWrapper(IMPRecipeProviderWrapper::new);
+    var btp = access.addProviderWrapper(IMPBlockTagProviderWrapper::new);
+
+    access.addProviderWrapper(
+      (DataProviderWrapper.LookupGeneratorAccessedFactory<
+        DataProviderWrapper<?>
+      >) (packOutput, lookup, generatorAccess) ->
+        new IMPItemTagProviderWrapper(packOutput, lookup, generatorAccess, btp)
+    );
+    access.addProviderWrapper(IMPPoiTypeTagProviderWrapper::new);
+    access.addProviderWrapper(packOutput ->
+      new DirectCopyProviderWrapper(
+        packOutput,
+        PackOutput.Target.DATA_PACK,
+        "patchouli_books",
+        access
+      )
+    );
+    access.addProviderWrapper(IMPBlockLootTableProviderWrapper::new);
+    access.addProviderWrapper(IMPAdvancementProviderWrapper::new);
+  }
 }
