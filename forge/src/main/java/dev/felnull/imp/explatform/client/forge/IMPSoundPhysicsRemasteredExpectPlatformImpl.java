76fd68e1e1a645528e00f2e475d60bcafe4b236f
Shadowbee27 <shadowbee.contact@proton.me>
2025-12-18 14:55:53 +0100

    3.23.1-1.20.1 Merge (#51)
    Reviewed-on: https://codeberg.org/Mod-Sauce/IamMusicPlayerRenewed/pulls/51
    Co-authored-by: Shadowbee27 <shadowbee.contact@proton.me>
    Co-committed-by: Shadowbee27 <shadowbee.contact@proton.me>

--- forge/src/main/java/dev/felnull/imp/explatform/client/forge/IMPSoundPhysicsRemasteredExpectPlatformImpl.java
+++ forge/src/main/java/dev/felnull/imp/explatform/client/forge/IMPSoundPhysicsRemasteredExpectPlatformImpl.java
@@ -1,20 +1,38 @@
 package dev.felnull.imp.explatform.client.forge;
 
-
 import com.sonicether.soundphysics.SoundPhysics;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundSource;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class IMPSoundPhysicsRemasteredExpectPlatformImpl {
-    public static void setDefaultEnvironment(int sourceID) {
-        SoundPhysics.setDefaultEnvironment(sourceID);
-    }
 
-    public static void setLastSoundCategoryAndName(SoundSource sc, String name) {
-        SoundPhysics.setLastSoundCategoryAndName(sc, name);
-    }
+  private static final Logger LOGGER = LogManager.getLogger(
+    IMPSoundPhysicsRemasteredExpectPlatformImpl.class
+  );
 
-    public static void onPlaySound(double posX, double posY, double posZ, int sourceID) {
-        SoundPhysics.onPlaySound(posX, posY, posZ, sourceID);
+  public static void setDefaultEnvironment(int sourceID) {
+    SoundPhysics.setDefaultEnvironment(sourceID);
+  }
+
+  public static void setLastSoundCategoryAndName(
+    SoundSource sc,
+    ResourceLocation name
+  ) {
+    try {
+      SoundPhysics.setLastSoundCategoryAndName(sc, name);
+    } catch (NoSuchMethodError e) {
+      LOGGER.error("Failed to load soundphysics intigration, due to: " + e);
     }
+  }
+
+  public static void onPlaySound(
+    double posX,
+    double posY,
+    double posZ,
+    int sourceID
+  ) {
+    SoundPhysics.onPlaySound(posX, posY, posZ, sourceID);
+  }
 }
-
