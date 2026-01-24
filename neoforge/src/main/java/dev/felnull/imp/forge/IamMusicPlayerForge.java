e370a7db08d5c77d0052f410c8c706f80dccab11
gly091020 <gulangyu1020@qq.com>
2026-01-24 16:56:18 +0800

    排除数据生成文件，修复构建脚本

---
+++ neoforge/src/main/java/dev/felnull/imp/forge/IamMusicPlayerForge.java
@@ -0,0 +1,23 @@
+package dev.felnull.imp.forge;
+
+import dev.felnull.imp.IamMusicPlayer;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+
+@Mod(IamMusicPlayer.MODID)
+public class IamMusicPlayerForge {
+
+  public IamMusicPlayerForge(IEventBus eventBus) {
+//    EventBuses.registerModEventBus(
+//      IamMusicPlayer.MODID,
+//      eventBus
+//    );
+    IamMusicPlayer.init();
+    eventBus.addListener(this::setup);
+  }
+
+  private void setup(FMLCommonSetupEvent e) {
+    IamMusicPlayer.setup();
+  }
+}
