42287ebccd5716dc1726025db15196e36a2ab188
gly091020 <gulangyu1020@qq.com>
2026-05-23 13:43:50 +0800

    Modify Boombox Controller

--- neoforge/src/main/java/dev/felnull/imp/create/IMPCreate.java
+++ neoforge/src/main/java/dev/felnull/imp/create/IMPCreate.java
@@ -6,6 +6,7 @@ import com.simibubi.create.api.behaviour.movement.MovementBehaviour;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.tterrag.registrate.util.entry.BlockEntityEntry;
 import com.tterrag.registrate.util.entry.BlockEntry;
+import com.tterrag.registrate.util.entry.ItemEntry;
 import dev.architectury.networking.NetworkManager;
 import dev.architectury.platform.Platform;
 import dev.architectury.registry.menu.MenuRegistry;
@@ -24,14 +25,20 @@ import dev.felnull.imp.create.container.MovingBoomboxContainer;
 import dev.felnull.imp.create.displaySource.*;
 import dev.felnull.imp.create.network.*;
 import dev.felnull.imp.create.pointType.IMPPointTypes;
+import dev.felnull.imp.item.IMPCreativeModeTabs;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.inventory.MenuType;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.Blocks;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.registries.RegisterEvent;
 import org.modsauce.otyacraftenginerenewed.util.OEMenuUtil;
+
+import java.util.List;
 
 public class IMPCreate {
     public static final CreateRegistrate REGISTRIES = CreateRegistrate.create(IamMusicPlayer.MODID);
@@ -49,8 +56,13 @@ public class IMPCreate {
     public static final RegistrySupplier<MenuType<BoomboxControllerMenu>> BOOMBOX_CONTROLLER_MENU = MENUS.register("boombox_controller", () -> OEMenuUtil.createMenuType(BoomboxControllerMenu::new));
     public static final BlockEntry<BoomboxControllerBlock> BOOMBOX_CONTROLLER_BLOCK = REGISTRIES.block("boombox_controller", BoomboxControllerBlock::new)
             .initialProperties(() -> Blocks.IRON_BLOCK)
-            .simpleItem()
             .register();
+    public static final ItemEntry<?> BOOMBOX_CONTROLLER_ITEM = REGISTRIES.item("boombox_controller", properties -> new BlockItem(BOOMBOX_CONTROLLER_BLOCK.get(), properties) {
+        @Override
+        public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltip, TooltipFlag flag) {
+            tooltip.add(Component.translatable("block.iammusicplayer.boombox_controller.tip").withStyle(ChatFormatting.GRAY));
+        }
+    }).register();
     public static final BlockEntityEntry<BoomboxControllerBlockEntity> BOOMBOX_CONTROLLER_BLOCK_ENTITY = REGISTRIES.blockEntity("boombox_controller", BoomboxControllerBlockEntity::new)
             .validBlock(BOOMBOX_CONTROLLER_BLOCK)
             .register();
@@ -69,6 +81,7 @@ public class IMPCreate {
         else
             NetworkManager.registerS2CPayloadType(ContraptionChangedPacketSTC.TYPE, ContraptionChangedPacketSTC.STREAM_CODEC);
         bus.addListener(IMPCreate::onRegistry);
+        bus.addListener(IMPCreate::onBuildCreativeTabs);
 
         REGISTRIES.displaySource("boombox_lyric", BoomboxLyricSource::new)
                 .onRegisterAfter(Registries.BLOCK_ENTITY_TYPE, s ->
@@ -105,6 +118,14 @@ public class IMPCreate {
                 new BoomboxMovingInteractionBehaviour()
             );
         });
+    }
+
+    public static void onBuildCreativeTabs(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == IMPCreativeModeTabs.MOD_TAB.getKey()) {
+            event.insertAfter(IMPBlocks.BOOMBOX.get().asItem().getDefaultInstance(),
+                    BOOMBOX_CONTROLLER_BLOCK.asStack(),
+                    CreativeModeTab.TabVisibility.PARENT_TAB_ONLY);
+        }
     }
 
     public static void onRegistry(RegisterEvent event){
